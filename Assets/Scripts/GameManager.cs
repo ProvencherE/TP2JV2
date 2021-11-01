e74876a1920c62a28853b764914148183997b7fb
Charles-Antoine Pouliot <charles-antoine2001@outlook.com>
2021-11-01 11:47:26 -0400

    création du game manager dans la scene

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,9 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
+    [SerializeField] private Text gameOverText;
     // Start is called before the first frame update
     void Start()
     {
