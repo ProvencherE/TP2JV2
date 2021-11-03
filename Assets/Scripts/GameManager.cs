9d789234336abc37a39f4ad4a7f670d8a829ab40
ProvencherE <71087778+ProvencherE@users.noreply.github.com>
2021-11-03 10:58:02 -0400

    Tower script and spawning system

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -6,15 +7,64 @@ using UnityEngine.UI;
 public class GameManager : MonoBehaviour
 {
     [SerializeField] private Text gameOverText;
+    [SerializeField] private float spawningRate;
+    [SerializeField] private int nbMaxWizard;
+
+    private float timerRespawn;
+    private List<GameObject> greenTowers;
+    private List<GameObject> blueTowers;
+    private GameObject[] greenWizards;
+    private GameObject[] blueWizards;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        timerRespawn = spawningRate;
+        greenTowers = new List<GameObject>(GameObject.FindGameObjectsWithTag("GreenTower"));
+        blueTowers = new List<GameObject>(GameObject.FindGameObjectsWithTag("BlueTower"));
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        spawn();
+    }
+
+    private void spawn()
+    {
+        if(timerRespawn > 0)
+        {
+            timerRespawn -= Time.deltaTime;
+        }else
+        {
+            GameObject tower = randomTower(greenTowers);
+            GameObject wizard = firstNotActive(greenWizards);
+            if(wizard != null) wizard.GetComponent<WizardManager>().spawn(tower.transform.position);
+            tower = randomTower(greenTowers);
+            wizard = firstNotActive(greenWizards);
+            if (wizard != null) wizard.GetComponent<WizardManager>().spawn(tower.transform.position);
+        }
+    }
+
+    private GameObject firstNotActive(GameObject[] wizards)
+    {
+        for(int i = 0; i < wizards.Length; i++)
+        {
+            if (!wizards[i].activeSelf)
+            {
+                return wizards[i];
+            }
+        }
+        return null;
+    }
+
+    private GameObject randomTower(List<GameObject> towers)
+    {
+        return towers[UnityEngine.Random.Range(0, towers.Count)];
+    }
+
+    public void towerDestroyed(GameObject towerDestroyed)
+    {
+        greenTowers.Remove(towerDestroyed);
     }
 }
